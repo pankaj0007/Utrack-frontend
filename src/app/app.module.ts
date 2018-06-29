dc617c79022737df1288760cd4c9707e35eb4b0b
Pankaj M <pankaj0707090@gmail.com>
2018-06-29 15:21:25 +0530

    Login functinality with Oauth JWT Web Token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Http, HttpModule} from '@angular/http';
+import {AuthConfig, AuthHttp} from 'angular2-jwt';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
-import { LoginComponent } from './login/login.component';
-import { UserComponent } from './user/user.component';
-import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {UserComponent} from './user/user.component';
+
+import {UserService} from './services/user.service';
+import {AuthenticationService} from './services/authentication.service';
+import {TOKEN_NAME} from './services/auth.constant';
+
+
+export function authHttpServiceFactory(http: Http) {
+  return new AuthHttp(new AuthConfig({
+    headerPrefix: 'Bearer',
+    tokenName: TOKEN_NAME,
+    globalHeaders: [{'Content-Type': 'application/json'}],
+    noJwtError: false,
+    noTokenScheme: true,
+    tokenGetter: (() => localStorage.getItem(TOKEN_NAME))
+  }), http);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: AuthHttp, useFactory: authHttpServiceFactory, deps: [Http]},
+    AuthenticationService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/services/auth.constant.ts
@@ -0,0 +1,3 @@
+export const TOKEN_AUTH_USERNAME = 'utrackAuth';
+export const TOKEN_AUTH_PASSWORD = 'XY7kmzoNzl100';
+export const TOKEN_NAME = 'access_token';
